81b963f4daeaf2c5f07e4872c160e8452a20e8a4
pnebuloni <pablo.nebuloni@gmail.com>
2025-03-23 20:04:10 -0300

    Update main_st.py

--- main_st.py
+++ main_st.py
@@ -1,11 +1,14 @@
 import streamlit as st
+# Esta llamada debe ser la primera función de Streamlit en ejecutarse
+st.set_page_config(page_title="EzOptions", layout="wide")
 import ezoptions_cc
 
-st.title("EzOptions en Streamlit Community Cloud")
-st.write("Presiona el botón para ejecutar la lógica principal.")
+def main():
+    st.title("EzOptions en Streamlit")
+    st.write("Bienvenido a la aplicación.")
 
-if st.button("Ejecutar EzOptions"):
-    st.info("Ejecutando la lógica principal de EzOptions...")
-    ezoptions_cc.main()
-    st.success("Proceso completado.")
-
+    if st.button("Ejecutar EzOptions"):
+        ezoptions_cc.main()
+        
+if __name__ == '__main__':
+    main()
